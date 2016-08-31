1376356c80a0e52619c9caa9c26f0380d4875ea8
kaikcreator <enriqueoriol@gmail.com>
2016-08-31 08:24:23 +0200

    18 - Using Alert Views

--- app/pages/lists/lists.ts
+++ app/pages/lists/lists.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { TodosPage } from '../todos/todos';
 
 /*
@@ -13,10 +13,35 @@ import { TodosPage } from '../todos/todos';
 })
 export class ListsPage {
 
-  constructor(private navCtrl: NavController) {}
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController) {}
 
   goToList(){
     this.navCtrl.push(TodosPage);
+  }
+
+  showAddList(){
+    let addListAlert = this.alertCtrl.create({
+      title: 'New list',
+      message: 'Give a name to the new list',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data =>{}
+        },
+        {
+          text:'Add',
+          handler: data => {this.goToList();}
+        }
+      ]
+    });
+
+    addListAlert.present();
   }
 
 }
